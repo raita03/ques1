a7c0801e19a7093c9068eb34769e348f5ed0fd12
raita03 <55320075+raita03@users.noreply.github.com>
2020-10-30 23:03:11 +0600

    Update and rename README.md to Que1.java

---
+++ Que1.java
@@ -0,0 +1,15 @@
+public class Que1 {
+
+    private static void customPrint(int n, String message) {
+        if (n > 0) {
+            System.out.println(message);
+            n--;
+            customPrint(n, message);
+        }
+    }
+
+    public static void main(String[] args) {
+        customPrint(5, “Hello, World!”);
+    }
+
+}
